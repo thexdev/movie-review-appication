88804a2c7643a7908f101528e089ba68ecd72198
thexdev <akbarnugroho253@gmail.com>
2019-12-13 00:38:34 +0700

    change native layout (css) movie item to bootstrap 4 css layout and card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, useState, useEffect, Suspense, lazy } from "react";
+import "./App.css";
+
+const Header = lazy(() => import("components/header"));
+const Movie = lazy(() => import("components/movie"));
+const Search = lazy(() => import("components/search"));
+const LoadIndicator = lazy(() => import("components/load-indicator"));
+const Error = lazy(() => import("components/error"));
+
+const OMDB_API_URL = "https://www.omdbapi.com/?apikey=476c7a19&s=";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(OMDB_API_URL + searchValue)
+      .then(response => response.json())
+      .then(omdb => {
+        if (omdb.Response === "True") {
+          setMovies(omdb.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(omdb.Error);
+          setLoading(false);
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetch(OMDB_API_URL + "man")
+      .then(response => response.json())
+      .then(omdb => {
+        setMovies(omdb.Search);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback="loading...">
+        <Header text="Movie Review" />
+        <Search search={search} />
+        <p className="App-intro">Sharing a few of our favourite movies</p>
+        <div className="container-fluid">
+          <div className="movies row justify-content-center">
+            {loading && !errorMessage ? (
+              <LoadIndicator />
+            ) : errorMessage ? (
+              <Error message={errorMessage} />
+            ) : (
+              movies.map(movie => <Movie key={movie.imdbID} movie={movie} />)
+            )}
+          </div>
+        </div>
+      </Suspense>
     </div>
   );
 }
 
-export default App;
+export default memo(App);
